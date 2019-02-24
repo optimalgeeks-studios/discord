a87e0413d96958c8d2ec3474f7aec280c77f8f76
IslandPro_Ninja <IslandProNinja@gmail.com>
2019-02-23 22:36:55 -0800

    Added botid
    2/23/19 - 10:36PM PST

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+//bot Imports\\
+const cmd = require("./commands")
+const logs = require("./logs");
+//Bot requirments\\
 const discord = require('discord.js')
 const client = new discord.Client()
 const config = require("./config.json");
